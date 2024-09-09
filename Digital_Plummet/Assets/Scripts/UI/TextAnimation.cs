6546bf361b726616ad94f1c8343cfa9859bc8683
Villegazs <alejitov214@hotmail.com>
2024-09-08 20:25:00 -0500

    Interfaz Tutorial
    Se implemento las animaciones para guiar a los nuevos usuarios

--- Digital_Plummet/Assets/Scripts/UI/TextAnimation.cs
+++ Digital_Plummet/Assets/Scripts/UI/TextAnimation.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using Unity.VisualScripting;
+using JetBrains.Annotations;
+using TMPro;
+using System;
 
 public class TextAnimation : MonoBehaviour
 {
+    [SerializeField] float fadeTime = 1.0f;
+    [SerializeField] Transform fallingCharacter;
+    [SerializeField] Transform handPressing;
+    [SerializeField] Transform handSliding;
+    [SerializeField] float movementY;
+    [SerializeField] float movementX;
     // Start is called before the first frame update
     void Start()
     {
-        
+        DOTween.Sequence()
+        .PrependInterval(1)
+        .Append(fallingCharacter.DOMoveY(fallingCharacter.position.y - movementY, fadeTime))
+        .Append(fallingCharacter.DOMoveY(fallingCharacter.position.y + movementY, 2*fadeTime));
+
+        DOTween.Sequence()
+        .PrependInterval(1)
+        .Append(handPressing.DOScale(0.5f, fadeTime))
+        .Append(handPressing.DOScale(1, fadeTime));
+
+        DOTween.Sequence()
+    .   PrependInterval(3*fadeTime)
+        .Append(handSliding.DOMoveX(- movementX, fadeTime))
+        .Append(handSliding.DOMoveX( movementX, 2 * fadeTime));
+
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
+
     }
 }
