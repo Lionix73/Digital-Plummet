e415637c6b8607bdbb767f9e178c7f91d9890174
Gagloger <140218770+Gagloger@users.noreply.github.com>
2024-10-06 13:04:31 -0500

    Saws pesudoAni, new tutroial, new PEM

--- Digital_Plummet/Assets/Scripts/Traps/EMPEffect.cs
+++ Digital_Plummet/Assets/Scripts/Traps/EMPEffect.cs
@@ -6,12 +6,21 @@ public class EMPEffect : MonoBehaviour
 {
     [Tooltip("How long the EMP affect the player")]
     [SerializeField] float effectDuration;
+    private GameObject playerPos;
+    private Vector3 pushForce;
+    [SerializeField] private float pushPower;
+
+    void Update(){
+        playerPos = GameObject.FindWithTag("Player");
+        pushForce=playerPos.transform.position-transform.position;
+        pushForce.Normalize();
+    }
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Player"){
             PlayerControllerV2 player = other.gameObject.GetComponent<PlayerControllerV2>();
             //Activacion de cosa que hace... falta hacer
 
-            player.EMPHit(effectDuration);
+            player.EMPHit(pushForce,pushPower);
         }
     }
 }
