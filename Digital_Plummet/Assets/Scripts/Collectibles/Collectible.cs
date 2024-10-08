77df570cc22e3a7048b01a029b5557d9f6e5bebe
santiCG <santiagocardenasgomez@gmail.com>
2024-10-07 22:33:41 -0500

    almojabana

--- Digital_Plummet/Assets/Scripts/Collectibles/Collectible.cs
+++ Digital_Plummet/Assets/Scripts/Collectibles/Collectible.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 public class Collectible : MonoBehaviour
 {
     [SerializeField] string collectibleID; // Unique ID for each collectible
-    [SerializeField] AudioClip collectSound;
     [SerializeField] GameObject collectEffect;
     private UIManager uIManager;
 
+    AudioSource sound;
+
     void Start()
     {
+        sound = GetComponent<AudioSource>();
+
         // Check if this collectible has already been collected
         if (PlayerPrefs.GetInt(collectibleID, 0) == 1)
         {
@@ -25,11 +28,6 @@ public class Collectible : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Collect();
-
-            if (collectSound != null)
-            {
-                AudioSource.PlayClipAtPoint(collectSound, transform.position);
-            }
 
             if (collectEffect != null)
             {
@@ -47,6 +45,8 @@ public class Collectible : MonoBehaviour
 
     private void Collect()
     {
+        sound.Play();
+
         // Add any effects on the player like increasing score, health, etc.
         CharacterManager playerScore = GameObject.Find("CharacterManager").GetComponent<CharacterManager>();
         playerScore.AddScore(1); // Add score or other effect
